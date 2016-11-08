b7e90424fb74ffc8a74e193f731dc2c3e778ada1
Kyle Thomson <Mila@Kyles-MacBook-Air.local>
2016-11-08 14:37:29 -0500

    finished checking guess

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,30 @@
 class Board
-  def initialize
-    draw_board
+  def initialize word_length
+    @chosen_letters = []
+    @misses = 6 
+    draw_new_board(word_length)
+    
   end
 
-  def draw_board
-    puts "you haven't chosen any letterrs yet."
+  def draw_new_board word_length
+    #puts "Chosen letters: " + @chosen_letters.join(", ")
+
+    word_length.times do
+      print "_ "
+    end
+    puts
+    puts "You have " + @misses.to_s + " chances left."
   end
+
+  def redraw_board guesses, guess, word_length
+    word_length.times do |i|
+      if i == 
+    puts guesses
+    puts guess
+  end
+
+
+
+
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -4,20 +4,23 @@ class Game
   
   def initialize
     define_the_word
-    board = Board.new
+    @board = Board.new(@word.length)
+    puts @word
+
   end
 
   def play
-    @word
+    guess = get_guess
+    @board.redraw_board(check_guess(guess), guess, @word.length)
   end
 
   def define_the_word
     dictionary = File.open("../dictionary.txt")
 
     loop do
-      possible_word = dictionary.readlines.sample     
+      possible_word = dictionary.readlines.sample.chomp   
       if possible_word.length > 4 && possible_word.length < 13
-        @word = possible_word
+        @word = possible_word.downcase
         dictionary.close
         return
       end
@@ -25,6 +28,15 @@ class Game
     end
   end
 
+  def get_guess
+    puts "Which letter do you guess? "
+    guess = gets.chomp
+    guess = guess.downcase
+  end
+
+  def check_guess guess
+    correct_guesses = (0 ... @word.length).find_all { |i| @word[i,1] == guess }  
+  end
 
 
 
